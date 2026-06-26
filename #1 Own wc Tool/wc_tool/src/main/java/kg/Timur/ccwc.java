51ba971feb33fe0f1064fbc8c49673f86ce849e5
Timur <timurmrztv@gmail.com>
2026-06-26 23:05:17 +0600

    add "-l" "-m" "-w" flags realization in #1 challenge

--- #1 Own wc Tool/wc_tool/src/main/java/kg/Timur/ccwc.java
+++ #1 Own wc Tool/wc_tool/src/main/java/kg/Timur/ccwc.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,15 +14,13 @@ public class ccwc {
             System.out.println("0 args");
         } else {
             boolean lFlag = false;
-            boolean cFlag = false;
             boolean mFlag = false;
+            boolean cFlag = false;
+            boolean wFlag = false;
             Map<File, String> fileStringMap = new HashMap<>();
             ArrayList<String> filePaths = new ArrayList<>();
             for (String arg : args) {
                 if (arg.charAt(0) != '-') {
-                    if(arg.charAt(arg.length()-1)=='\\'){
-
-                    }
                     filePaths.add(arg);
                 } else {
                     for (char c : arg.toCharArray()) {
@@ -31,6 +30,8 @@ public class ccwc {
                             cFlag = true;
                         } else if (c == 'm') {
                             mFlag = true;
+                        } else if (c == 'w') {
+                            wFlag = true;
                         }
                     }
                 }
@@ -45,11 +46,25 @@ public class ccwc {
             }
             for (File file : filesArray) {
                 try {
+                    System.out.print('\t');
                     if (cFlag) {
-                        FileInputStream fileInputStream = new FileInputStream(file);
-                        System.out.print(fileInputStream.available() + " ");
-                        System.out.println(fileStringMap.get(file));
+                        cFlag(file);
                     }
+                    if (lFlag) {
+                       lFlag(file);
+                    }
+                    if (wFlag) {
+                       wFlag(file);
+                    }
+                    if (mFlag){
+                        mFlag(file);
+                    }
+                    if (!cFlag && !lFlag && !wFlag && !mFlag){
+                        lFlag(file);
+                        wFlag(file);
+                        cFlag(file);
+                    }
+                    System.out.println(fileStringMap.get(file));
                 } catch (FileNotFoundException e) {
                     throw new RuntimeException(e);
                 } catch (IOException e) {
@@ -60,4 +75,25 @@ public class ccwc {
 
         }
     }
+
+    private static void cFlag(File file) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(file);
+        System.out.print(fileInputStream.available() + " ");
+    }
+
+    private static void lFlag(File file) throws IOException {
+        FileReader fileReader = new FileReader(file);
+        System.out.print(fileReader.readAllLines().size() + " ");
+    }
+
+    private static void wFlag(File file) throws IOException {
+        FileReader fileReader = new FileReader(file);
+        String text = fileReader.readAllAsString();
+        System.out.print(Arrays.stream(text.split("\\s+")).toArray().length + " ");
+    }
+
+   private static void mFlag(File file) throws IOException {
+       FileReader fileReader = new FileReader(file);
+       System.out.print(fileReader.readAllAsString().length() + " ");
+   }
 }
