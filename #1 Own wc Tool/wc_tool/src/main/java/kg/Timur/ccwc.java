ddc69484555f3c850104a1fba75ce80b3a35bff6
Timur <timurmrztv@gmail.com>
2026-06-27 22:51:46 +0600

    Finish #1 ccwc tool

--- #1 Own wc Tool/wc_tool/src/main/java/kg/Timur/ccwc.java
+++ #1 Own wc Tool/wc_tool/src/main/java/kg/Timur/ccwc.java
@@ -1,41 +1,43 @@
 package kg.Timur;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ccwc {
-    public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("0 args");
-        } else {
-            boolean lFlag = false;
-            boolean mFlag = false;
-            boolean cFlag = false;
-            boolean wFlag = false;
-            Map<File, String> fileStringMap = new HashMap<>();
-            ArrayList<String> filePaths = new ArrayList<>();
-            for (String arg : args) {
-                if (arg.charAt(0) != '-') {
-                    filePaths.add(arg);
-                } else {
-                    for (char c : arg.toCharArray()) {
-                        if (c == 'l') {
-                            lFlag = true;
-                        } else if (c == 'c') {
-                            cFlag = true;
-                        } else if (c == 'm') {
-                            mFlag = true;
-                        } else if (c == 'w') {
-                            wFlag = true;
-                        }
+    public static void main(String[] args) throws IOException {
+
+        boolean lFlag = false;
+        boolean mFlag = false;
+        boolean cFlag = false;
+        boolean wFlag = false;
+        Map<File, String> fileStringMap = new HashMap<>();
+        ArrayList<String> filePaths = new ArrayList<>();
+
+
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            if (i == 0 && arg.charAt(0) == '-') {
+                for (char c : arg.toCharArray()) {
+                    if (c == 'l') {
+                        lFlag = true;
+                    } else if (c == 'c') {
+                        cFlag = true;
+                    } else if (c == 'm') {
+                        mFlag = true;
+                    } else if (c == 'w') {
+                        wFlag = true;
                     }
                 }
+            } else {
+                filePaths.add(arg);
             }
+        }
+
+
+        if (!filePaths.isEmpty()) {
             ArrayList<File> filesArray = new ArrayList<>();
             for (String filePath : filePaths) {
                 Path path = Paths.get(filePath);
@@ -45,34 +47,59 @@ public class ccwc {
                 }
             }
             for (File file : filesArray) {
-                try {
+                System.out.print('\t');
+                if (cFlag) {
+                    cFlag(file);
+                }
+                if (lFlag) {
+                    lFlag(file);
+                }
+                if (wFlag) {
+                    wFlag(file);
+                }
+                if (mFlag) {
+                    mFlag(file);
+                }
+                if (!cFlag && !lFlag && !wFlag && !mFlag) {
+                    lFlag(file);
+                    wFlag(file);
+                    cFlag(file);
+                }
+                System.out.println(fileStringMap.get(file));
+            }
+
+
+        } else {
+            if (System.in.available() > 0) {
+                byte[] bytes = System.in.readAllBytes();
+                String fileContent = new String(bytes, StandardCharsets.UTF_8);
+                File file = new File("/tmp/test");
+                file.createNewFile();
+                FileWriter fileWriter = new FileWriter(file);
+                fileWriter.write(fileContent);
+                fileWriter.close();
+
+                if(cFlag || wFlag || lFlag || mFlag){
                     System.out.print('\t');
                     if (cFlag) {
                         cFlag(file);
                     }
                     if (lFlag) {
-                       lFlag(file);
+                        lFlag(file);
                     }
                     if (wFlag) {
-                       wFlag(file);
+                        wFlag(file);
                     }
-                    if (mFlag){
+                    if (mFlag) {
                         mFlag(file);
                     }
-                    if (!cFlag && !lFlag && !wFlag && !mFlag){
-                        lFlag(file);
-                        wFlag(file);
-                        cFlag(file);
-                    }
-                    System.out.println(fileStringMap.get(file));
-                } catch (FileNotFoundException e) {
-                    throw new RuntimeException(e);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
+                } else {
+                    lFlag(file);
+                    wFlag(file);
+                    cFlag(file);
                 }
-
+                System.out.println();
             }
-
         }
     }
 
@@ -92,8 +119,8 @@ public class ccwc {
         System.out.print(Arrays.stream(text.split("\\s+")).toArray().length + " ");
     }
 
-   private static void mFlag(File file) throws IOException {
-       FileReader fileReader = new FileReader(file);
-       System.out.print(fileReader.readAllAsString().length() + " ");
-   }
+    private static void mFlag(File file) throws IOException {
+        FileReader fileReader = new FileReader(file);
+        System.out.print(fileReader.readAllAsString().length() + " ");
+    }
 }
--- #1 Own wc Tool/wc_tool/src/test/java/kg/Timur/ccwcTest.java
+++ #1 Own wc Tool/wc_tool/src/test/java/kg/Timur/ccwcTest.java
@@ -1,11 +1,13 @@
 package kg.Timur;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ccwcTest extends ccwc {
 
     @org.junit.jupiter.api.Test
-    void testMain() {
+    void testMain() throws IOException {
         main(new String[]{"-c", "test.txt src/main/resources/"});
     }
 }
